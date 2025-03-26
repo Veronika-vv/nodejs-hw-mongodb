abcea9653c84b0e1ee46c3e2aa034551ad5c7062
Veronika Voitovska <v.voitovska@gmail.com>
2025-03-26 13:26:49 +0200

    git add hw4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { initMongoConnection } from './db/initMongoDB.js';
 import { setupServer } from './server.js';
 
-async function Start() {
+const bootstrap = async () => {
   await initMongoConnection();
   setupServer();
-}
+};
 
-Start();
+bootstrap();
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,41 @@
 import { contactsCollection } from '../db/models/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getAllContacts = () => contactsCollection.find();
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = page > 0 ? (page - 1) * perPage : 0;
+
+  const contactsQuery = contactsCollection.find();
+
+  if (typeof filter.type !== 'undefined') {
+    contactsQuery.where('contactType').equals(filter.type);
+  }
+  if (typeof filter.isFavourite !== 'undefined') {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const [contactsCount, contacts] = await Promise.all([
+    contactsCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+  return {
+    data: contacts,
+    ...paginationData,
+  };
+};
 
 export const getContactById = (contactId) =>
   contactsCollection.findById(contactId);
