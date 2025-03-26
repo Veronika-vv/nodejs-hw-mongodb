b623cacb3844409798d0e35b32371e893121e246
Veronika Voitovska <v.voitovska@gmail.com>
2025-03-26 13:35:40 +0200

    correct

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -7,3 +7,11 @@ export const createContactSchema = Joi.object({
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  phoneNumber: Joi.string().min(10).max(14),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
