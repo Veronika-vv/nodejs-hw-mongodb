6ee8aa9ddf8d3f116fae67cb977eeab472be205b
Veronika Voitovska <v.voitovska@gmail.com>
2025-04-11 09:50:56 +0300

    hw7

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,8 +4,8 @@ export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.name,
-      data: err,
+      message: err.message,
+      data: err.name,
     });
     return;
   }
